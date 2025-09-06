d0d23169c61a8509bab3f868a3393d03ccd1d519
Kairav Shaurya Makhija <krex828@gmail.com>
2025-09-06 11:14:24 +0800

    something

--- test_princess.py
+++ test_princess.py
@@ -5,12 +5,16 @@ data = {
   "tasks": [
     { "name": "A", "start": 480, "end": 540, "station": 1, "score": 2 },
     { "name": "B", "start": 600, "end": 660, "station": 2, "score": 1 },
-    { "name": "C", "start": 720, "end": 780, "station": 3, "score": 3 }
+    { "name": "C", "start": 720, "end": 780, "station": 3, "score": 3 },
+    { "name": "D", "start": 840, "end": 900, "station": 4, "score": 1 },
+    { "name": "E", "start": 960, "end": 1020, "station": 1, "score": 4 },
+    { "name": "F", "start": 530, "end": 590, "station": 2, "score": 1 }
   ],
   "subway": [
     { "connection": [0, 1], "fee": 10 },
     { "connection": [1, 2], "fee": 10 },
-    { "connection": [2, 3], "fee": 20 }
+    { "connection": [2, 3], "fee": 20 },
+    { "connection": [3, 4], "fee": 30 }
   ],
   "starting_station": 0
 }
